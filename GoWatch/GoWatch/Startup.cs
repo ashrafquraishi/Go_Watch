e4a508eb9e783ef20e592e2d528cd20457a2012c
ashraf quraishi <ashrafquraishi4@gmail.com>
2019-01-22 14:30:20 -0600

    initial commit

---
+++ GoWatch/GoWatch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoWatch.Startup))]
+namespace GoWatch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
